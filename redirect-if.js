d6baf00699543a958b0c840707044f04ad7819a4
BDtetra <bdtetra@gmail.com>
2025-11-20 04:19:04 +0900

    Update redirect-if.js

--- redirect-if.js
+++ redirect-if.js
@@ -3,27 +3,33 @@ redirect-if.js text/javascript
 // Arguments: pattern (regex string), targetFunction (JS code string)
 
 // redirect-if scriptlet for uBlock Origin
-(function() => {
+(function() {
     'use strict';
 
-    // Register the scriptlet so uBO can use it by name
-    self.redirectIf = function(pattern, replacement) {
+    // The function returned by this IIFE becomes the scriptlet implementation.
+    return function(pattern, replacementTemplate) {
         try {
-            const regex = new RegExp(pattern);
-            const url = window.location.href;
+            if (!pattern || !replacementTemplate) return;
 
-            const match = url.match(regex);
+            // pattern is passed as a string from the filter, create RegExp
+            var re = new RegExp(pattern);
+            var url = window.location.href;
+            var match = url.match(re);
             if (!match) return;
 
-            const newUrl = url.replace(regex, replacement);
+            // Replace $1, $2, ... in the replacementTemplate with the capture groups
+            var newUrl = replacementTemplate.replace(/\$([0-9]+)/g, function(_, n) {
+                return typeof match[Number(n)] !== 'undefined' ? match[Number(n)] : '';
+            });
 
-            // Prevent infinite reload loops
-            if (newUrl !== url) {
-                window.location.replace(newUrl);
-            }
-        } catch (e) {
-            console.error('redirect-if scriptlet error:', e);
+            // If replacementTemplate doesn't produce an absolute URL, you may get a bad URL.
+            // It's safest to pass a full absolute URL in the filter (see examples below).
+            if (!newUrl || newUrl === url) return;
+
+            // Redirect (use replace so it doesn't create an extra history entry)
+            window.location.replace(newUrl);
+        } catch (err) {
+            console.error('redirect-if scriptlet error:', err);
         }
     };
-
 })();
