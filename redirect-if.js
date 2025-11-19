cdffb36daa1718aec574db65bb809982dd40aa78
BDtetra <bdtetra@gmail.com>
2025-11-19 22:09:25 +0900

    Create redirect-if.js

---
+++ redirect-if.js
@@ -0,0 +1,17 @@
+// redirect-if scriptlet for uBlock Origin
+// Arguments: pattern (regex string), targetFunction (JS code string)
+
+(function() {
+    function redirectIf(pattern, fnBody) {
+        let re = new RegExp(pattern);
+        let m = re.exec(location.href);
+        if (m) {
+            // create function so the filter can pass in code
+            let fn = new Function('match', fnBody);
+            fn(m);
+        }
+    }
+
+    // Expose this scriptlet name to uBO
+    return redirectIf;
+})();
